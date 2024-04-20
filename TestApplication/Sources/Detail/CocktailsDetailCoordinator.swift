d39d9552e6f91c3ead3775785d47bf50bd3474fb
techman <Sharapat.9828@gmail.com>
2024-04-20 23:11:36 +0500

    feat: added icons for details

---
+++ TestApplication/Sources/Detail/CocktailsDetailCoordinator.swift
@@ -0,0 +1,32 @@
+//
+//  CocktailsDetailCoordinator.swift
+//  TestApplication
+//
+//  Created by Sharapat Azamat on 20.04.2024.
+//
+
+import Foundation
+import Swinject
+import UIKit
+
+protocol CocktailsDetailRoutes {
+}
+
+class CocktailsDetailRouter: Router<UIViewController>, CocktailsDetailRoutes {
+    private var resolver: Resolver {
+        return AppDependencyManager.shared.resolver
+    }
+}
+
+class CocktailsDetailCoordinator: Coordinator<CocktailsDetailRouter> {
+    init(resolver: Resolver, detailID: String) {
+        let router = CocktailsDetailRouter()
+        let vm = CocktailsDetailViewModel(container: .init(routes: router, detailID: detailID))
+        let vc = CocktailsDetailViewController(container: .init(viewModel: vm))
+        router.viewController = vc
+
+        super.init(resolver: resolver, router: router, view: vc)
+
+        vc.coordinator = self
+    }
+}
